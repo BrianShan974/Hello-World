4bd890a9c8a7dd7a749e9e51aae6e602e1ee4a6a
BrianShan974 <50822674+BrianShan974@users.noreply.github.com>
2019-05-26 22:41:11 +0800

    Delete problem1.py

--- problem1.py
+++
@@ -1,6 +0,0 @@
-def balance(initialBalance, annualInterestRate, monthlyPaymentRate):
-    monthlyInterestRate = annualInterestRate / 12
-    for month in range(12):
-        initialBalance -= monthlyPaymentRate * initialBalance
-        updatedBalance = (1 + monthlyInterestRate) * initialBalance
-    return updatedBalance
