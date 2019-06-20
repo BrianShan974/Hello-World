ad7d2330438188d119a06fbdcae53171b416ca5e
BrianShan974 <50822674+BrianShan974@users.noreply.github.com>
2019-06-20 20:21:03 +0800

    Delete problem 2.py

--- problem 2.py
+++
@@ -1,9 +0,0 @@
-def bobcheck(string):
-    count = 0
-    length = len(string)
-    for start in range(length - 2):
-        if string[start: start + 3] == "bob":
-            count += 1
-    return count
-
-print(bobcheck(input("Please enter the string:")))
