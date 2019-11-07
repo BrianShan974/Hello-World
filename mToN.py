c79024c331ad4b4d235e463c6aeedbf0086194fd
BrianShan974 <50822674+BrianShan974@users.noreply.github.com>
2019-11-07 11:23:18 +0800

    Add files via upload

---
+++ mToN.py
@@ -0,0 +1,43 @@
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+mAndDs = [str(i) for i in range(1, 17)]
+
+m = eval(input("Enter the base you want to convert from:\n"))
+n = eval(input("Enter the base you want to convert to:\n"))
+
+number = input("Enter the number:\n")
+
+output = ""
+
+def index(List, item):
+	for i in range(len(List)):
+		if List[i] == item:
+			return i
+
+def denary(string, m):
+	output = 0
+	for i in range(len(string)):
+		output += (m ** i) * index(digits, string[-i])
+	return output
+
+numberDenary = denary(number, m)
+
+def log(base, number):
+	i = 0
+	while base ** i < number:
+		i += 1
+	if base ** i == number:
+		return i
+	else:
+		return i - 1
+
+digit = 1
+
+if n == 10:
+	print(numberDenary)
+else:
+	while digit != 0:
+		theHighestPowerBelow = n ** (log(n, numberDenary) + 1)
+		digit = numberDenary // theHighestPowerBelow
+		output = digits[digit] + output
+		numberDenary %= theHighestPowerBelow
+	print(output)
